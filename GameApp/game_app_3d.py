6e8a0596f70d0481465f9d638e244630bb2e9337
Hans Lo <hansshulo@gmail.com>
2011-12-15 23:48:30 -0500

    Remove legacy code.
    Version Control enables us to get rid of old code with no risk.

--- GameApp/game_app_3d.py
+++ GameApp/game_app_3d.py
@@ -18,38 +18,15 @@ from direct.showbase.ShowBase import ShowBase
 from pandac.PandaModules import CardMaker
 from direct.task import Task
 
-from math import pi, sin, cos
-
 import wall_layout
 import floor_layout
 
-#old stuff
-import sys
-from characters import *
-from environment import *
-from buildings import *
-from terrain_objects import *
-from object_3d import *
-import os
-from os import getcwd, path
-from constants import *
-from misc_ui import *
-from game_assets import *
-from buildings import *
-from system import *
-from lavida_gui import *
-
-os.environ['SDL_VIDEO_CENTERED'] = '1'
-
-NORMAL_MODE = 1
-EDIT_MODE = 2
-
 MAP_SIZE = 20
-
 
 class GameApp3d(ShowBase):
     """
-    The main game class which manages all other processes
+    The main game class which manages all other processes.
+    Currently handles graphics.
     """
     def __init__(self):
                 
@@ -62,50 +39,6 @@ class GameApp3d(ShowBase):
         
         self.selected_object = None
         self.edit_terrain = False
-
-    def SetupGUI( self ):
-        self.pie_menu = StandardActionsMenu( self.root, self.m_EventQueue )
-        self.pie_menu.visible = 0
-
-    def SetupEventQueue( self ):
-        self.m_EventQueue = EventQueue()
-        
-    def SetupLighting( self ):
-        '''
-        Setup OpenGL and some variable relating to lighting
-        '''
-        LightAmbient  = ( 0.2, 0.2, 0.2, 1.0 )
-        LightDiffuse  = ( 0.8, 0.8, 0.8, 1.0 )
-        self.LightPosition = [ 10.0, 50.0, 30, 1.0 ]
-        LightSpecular = ( 1.0, 1.0, 1.0, 1.0 )
-
-        self.light = soya.Light( self )
-        self.light.set_xyz( 10.0, 50.0, 30 )
-        self.light.ambient = LightAmbient
-        self.light.diffuse = LightDiffuse
-        self.light.specular = LightSpecular
-
-
-    def SetupFog( self ):
-        '''
-        Setup OpenGL fog
-        '''       
-        self.atmosphere = soya.Atmosphere()
-        self.atmosphere.fog = 1
-        self.atmosphere.fog_start = 70.0
-        self.atmosphere.fog_end = 100.0
-        self.atmosphere.fog_color = ( 0.7, 0.7, 1.0, 1.0 )
-        self.atmosphere.bg_color= ( 0.7, 0.7, 1.0, 1.0 )
-        self.atmosphere.ambient = ( 0.3, 0.3, 0.3, 1.0 )
-
-
-    def SetupKeyBuffer( self ):
-        '''
-        Setup the keyboard buffer
-        '''
-        self.m_KeyBuffer = []
-        for i in range(320):
-            self.m_KeyBuffer.append( False )
 
     def load_graphics( self ):
         '''load all 3d objects and models'''
@@ -182,114 +115,4 @@ class GameApp3d(ShowBase):
         back.setH(180)
         back.setPos(x+1,y,0)
 
-    def begin_round( self ):
-        soya.Body.begin_round( self )
-        self.ProcessEvents()
-        self.ProccessKeys()
-
-    def ProcessEvents(self):
-        '''
-        Process all game events
-        '''           
-
-        for event in soya.MAIN_LOOP.events:
-            if event[ 0 ] == soya.sdlconst.KEYDOWN:
-                self.m_KeyBuffer[ event[ 1 ] ] = True
-
-            elif event[0] == soya.sdlconst.KEYUP:
-                self.m_KeyBuffer[ event[ 1 ] ] = False
-
-            elif event[ 0 ] == soya.sdlconst.QUIT:
-                soya.MAIN_LOOP.stop()
-
-            elif event[0] == soya.sdlconst.MOUSEMOTION:
-                self.mouse_pos = self.m_Camera.coord2d_to_3d( event[1], event[2], -15.0 )
-                self.move( self.mouse_pos )
-              
-            elif event[0] == soya.sdlconst.MOUSEBUTTONDOWN:
-                if event[1] == soya.sdlconst.BUTTON_LEFT:
-                    self.pie_menu.visible = 0
-                if event[1] == soya.sdlconst.BUTTON_RIGHT:
-                    self.pie_menu.visible = 1
-                    self.pie_menu.move( event[2], event[3] )
-                    
-            
-            for event in self.m_EventQueue.events:
-                if event.category == ACTION:
-                    if not event.prepared:
-                        self.PrepareActionEvent( event )
-
-    def PrepareActionEvent( self, event ):
-        if event.name == "move_to":
-            data = event.GetCurrentAction().data[ 0 ]
-            mouse = self.m_Camera.coord2d_to_3d( data[0], data[1] )
-            result = self.raypick( self.m_Camera, self.m_Camera.vector_to( mouse ) )
-            impact, normal = result
-            event.GetCurrentAction().data[ 0 ] = impact
-            event.prepared = True
-
-    def ProccessKeys( self ):
-        '''
-        Process the assigned keys and trigger behaviours
-        '''
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_ESCAPE ]:
-            soya.MAIN_LOOP.stop()
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_d ]:
-            self.m_Camera.speed.x = 1.0
-        elif self.m_KeyBuffer[ soya.sdlconst.K_a ]:
-            self.m_Camera.speed.x = -1.0
-        else:
-            self.m_Camera.speed.x /= 2.0
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_s ]:
-            self.m_Camera.speed.z = 1.0
-        elif self.m_KeyBuffer[ soya.sdlconst.K_w ]:
-            self.m_Camera.speed.z = -1.0
-        else:
-            self.m_Camera.speed.z /= 2.0
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_f ]:
-            self.m_Camera.speed.y = -0.3
-        elif self.m_KeyBuffer[ soya.sdlconst.K_r ]:
-            self.m_Camera.speed.y = 0.3
-        else:
-            self.m_Camera.speed.y /= 2.0
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_DOWN ]:
-            self.m_Camera.rot_x = -3.0
-        elif self.m_KeyBuffer[ soya.sdlconst.K_UP ]:
-            self.m_Camera.rot_x = 3.0
-        else:
-            self.m_Camera.rot_x /= 1.5
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_LEFT ]:
-            self.m_Camera.rot_y = 3.0
-        elif self.m_KeyBuffer[ soya.sdlconst.K_RIGHT ]:
-            self.m_Camera.rot_y = -3.0
-        else:
-            self.m_Camera.rot_y /= 1.5
-
-        if self.m_KeyBuffer[ soya.sdlconst.K_z ]:
-            self.m_Camera.rot_z = 3.0
-        elif self.m_KeyBuffer[ soya.sdlconst.K_x ]:
-            self.m_Camera.rot_z = -3.0
-        else:
-            self.m_Camera.rot_z /= 1.5
-
-    def PrintToConsole(self, a_Message, a_LineNumber ):
-        '''
-        Print a message to the console on a specific line
-        @param a_Message String : a message to display
-        @param a_LineNumber Integer: the line to pront your message
-        '''
-
-    def AddMessage( self, a_Message ):
-        '''
-        Add a message to the console message queue
-        @param a_Message String : a message to display
-        '''
-        print "%s\n" % a_Message
-
-
+    # TODO: Event handling
