0b14b8c7122ad74d34925c5c7adc64f54dc53da4
Hans Lo <hansshulo@gmail.com>
2011-12-15 20:33:02 -0500

    Begin moving from Soya3D to Panda3D
    Rationale is that Panda3D has much better support and a much better
    community. Eventually we need to encapsulate Panda3D calls underneath
    custom modules so that if we ever need to change libraries (again) in
    the future, it won't be so painful. For now getting it working is a
    priority. With this commit, a simple scene with an oven and grass
    ground is visible.

--- GameApp/game_app_3d.py
+++ GameApp/game_app_3d.py
@@ -14,8 +14,14 @@
  *
 '''
 
+from direct.showbase.ShowBase import ShowBase
+from pandac.PandaModules import CardMaker
+from direct.task import Task
+
+from math import pi, sin, cos
+
+#old stuff
 import sys
-from ogl_camera import *
 from characters import *
 from environment import *
 from buildings import *
@@ -28,72 +34,40 @@ from misc_ui import *
 from game_assets import *
 from buildings import *
 from system import *
-from lavida_gui import * 
-from Cookers import Oven
-import soya, soya.widget
-import soya.pudding as pudding
-import soya.pudding.ext.fpslabel
-
+from lavida_gui import *
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 NORMAL_MODE = 1
 EDIT_MODE = 2
 
-class GameApp3d( soya.World ):
+MAP_SIZE = 20
+
+
+class GameApp3d(ShowBase):
     """
     The main game class which manages all other processes
-    @breif A 3d GameApp
     """
-    def __init__(self, a_ViewPortWidth=1024, a_ViewPortHeight=768, a_Fullscreen=False, a_AppName="GameApp3d", a_DataPath=None):
-        '''
-        Construct the Game app object and load game assets for startup
-        @param a_ViewPortWidth Int: the width of the viewport
-        @param a_ViewPortHeight Int: the height of the viewport
-        @param a_Fullscreen Boolean: Display full screen True or False
-        @param a_AppName String: The title to show on the window in windowed mode
-        @param a_DataPath String: The path to the data directory
-        @return returns a GameApp3d instance
-        '''
-        
-        soya.init( a_AppName, a_ViewPortWidth, a_ViewPortHeight, a_Fullscreen )
-        	
-        soya.World.__init__( self )
-
-        self.DATA_PATH = a_DataPath != None and a_DataPath or path.join( getcwd(), "data" )
-
-        self.m_Camera = oglCamera( self )
-        self.m_Camera.set_xyz( 37.0, 10.0, 23.0 )
-        self.m_Camera.look_at( soya.Point( self, 16.0, -6.0, 10.0) )
-
-        self.m_Camera.add_vector( soya.Vector( None, 10.0, 0.0, 0.0 ) )
-
-        self.m_Camera.back = 100
-        
-        self.root = soya.gui.RootLayer(None)
-        soya.gui.CameraViewport( self.root, self.m_Camera )
-
-        soya.set_root_widget( self.root )
+    def __init__(self):
                 
-        #pudding.ext.fpslabel.FPSLabel( soya.root_widget, position = pudding.TOP_RIGHT )
-
-        self.m_SelectedObject = None
-
-        self.m_EditTerrain = False
-
-        self.SetupLighting()
-
-        self.SetupFog()
-
-        self.SetupKeyBuffer()
+        ShowBase.__init__(self)
+        #initialize camera
+        self.disableMouse()
+        self.load_graphics()
+        self.camera.setPos(-15,-15,10)
+        self.camera.setHpr(-40,-25,0)
         
-        self.SetupEventQueue()
+        #initialize screen
+        #initialize gui
         
-        self.SetupGUI()
+        self.selected_object = None
+        self.edit_terrain = False
+        #self.SetupLighting()
+        #self.SetupFog()
+        #self.SetupKeyBuffer()
+        #self.SetupEventQueue()
+        #self.SetupGUI()
 
-        self.LoadObjects()
-        
-        
     def SetupGUI( self ):
         self.pie_menu = StandardActionsMenu( self.root, self.m_EventQueue )
         self.pie_menu.visible = 0
@@ -138,55 +112,29 @@ class GameApp3d( soya.World ):
         for i in range(320):
             self.m_KeyBuffer.append( False )
 
-    def LoadObjects( self ):
+    def load_graphics( self ):
         '''load all 3d objects and models'''
-        self.objects = []; oadd = self.objects.append
-
         # floor terrain
-        terrain_size = 50 * 3
-        
-        self.terrain = soya.Terrain( self, terrain_size, terrain_size )		  
-		  
-        grass = soya.Material.get( "grass" )
-        grass.mip_map = 1
-        self.terrain.set_material_layer( grass, 0.0, 1.0 )
-        self.terrain.patch_size = 10.0
-        self.terrain.texture_factor = 0.50
-        
-        
+        cm = CardMaker("floor")
+        terrain_size = MAP_SIZE/2
+        cm.setFrame(-terrain_size, terrain_size, -terrain_size, terrain_size)
+        card = cm.generate()
+        floor = self.render.attachNewNode(card)
+        floor.setP(270)
+        grass_texture = self.loader.loadTexture("./data/images/grass.png")
+        floor.setTexture(grass_texture)
 
-        self.AssetManager = AssetManager( self, "environment/manifests/default.man", self.DATA_PATH )
+        # TODO: system to load all objects as specified from a file
+        self.oven = self.loader.loadModel("./data/egg/oven")
+        self.oven.reparentTo(self.render)
+        self.oven.setScale(1, 1, 1)
+        self.oven.setPos(0, 0, 1)
 
-##        f = LoadableRegion( self, a_AssetManager=self.AssetManager )
-##        f.Load( "%s/environment/floors/first.flr" % self.DATA_PATH )
-        #f.Save( "%s/environment/floors/first.pkl" % self.DATA_PATH )
-        pickle_file = open( "%s/environment/floors/first.pkl" % self.DATA_PATH, "r" )
-        f = pickle.load( pickle_file )
-        pickle_file.close()
-        
-        self.grid = g = f.CreateGrid()
-             
-        model_builder = soya.SimpleModelBuilder()
-        model_builder.shadow = 1
-        
-        b = soya.Body( self, f.to_model() )
-        b.y = 0.01 
-        
+        # TODO: system to load walls
 
-        self.sorcerer = Avatar( self, DUDE, self.AssetManager, self.m_EventQueue, [ self.grid ] )
-        self.sorcerer.selected = True
-        self.sorcerer.x = ( 25 * 3 ) + 1.5
-        self.sorcerer.z = ( 37 * 3 ) + 1.5
+        # TODO: system to load floors
         
-        p = PathFinder( g, g.GridSpaceCoords( self.sorcerer.x, self.sorcerer.z, WALL_WIDTH, 1.5 ) , [( 30, 30 )] )
-               
-        self.sorcerer.UseWaypoints( p.path )
-        
-        self.m_Camera.look_at( self.sorcerer )
-        
-        self.oven = Oven( self, self.root , self.m_EventQueue )
-        
-        self.oven.SetPosition( ( 25 * 3 ) + 1.5, 0, ( 25 * 3 ) + 1.5 )
+        # TODO: characters
 
     def begin_round( self ):
         soya.Body.begin_round( self )
@@ -213,7 +161,6 @@ class GameApp3d( soya.World ):
                 self.move( self.mouse_pos )
               
             elif event[0] == soya.sdlconst.MOUSEBUTTONDOWN:
-                # self.pie_menu.on_mouse_pressed( event[1], event[2], event[3] )
                 if event[1] == soya.sdlconst.BUTTON_LEFT:
                     self.pie_menu.visible = 0
                 if event[1] == soya.sdlconst.BUTTON_RIGHT:
@@ -225,19 +172,6 @@ class GameApp3d( soya.World ):
                 if event.category == ACTION:
                     if not event.prepared:
                         self.PrepareActionEvent( event )
-##              mouse = self.m_Camera.coord2d_to_3d(event[2], event[3])
-##              result = self.raypick( self.m_Camera, self.m_Camera.vector_to( mouse ) )
-##              
-##              impact, normal = result
-##              g = self.grid
-##              g_coord = g.GridSpaceCoords
-##              p = PathFinder( g, g_coord( self.sorcerer.x, self.sorcerer.z, WALL_WIDTH ),
-##                              [ g_coord( impact.x, impact.z, WALL_WIDTH ) ] )
-##              
-##              self.sorcerer.UseWaypoints( p.path )
-
-
-
 
     def PrepareActionEvent( self, event ):
         if event.name == "move_to":
@@ -298,117 +232,12 @@ class GameApp3d( soya.World ):
         else:
             self.m_Camera.rot_z /= 1.5
 
-##        if self.m_KeyBuffer[ soya.sdlconst.K_d ]:
-##            self.m_Camera.speed.x = -1.0
-##        elif self.m_KeyBuffer[ soya.sdlconst.K_a ]:
-##            self.m_Camera.speed.x = 1.0
-##        else:
-##            self.m_Camera.speed.x = 0.0
-##
-##        if self.m_KeyBuffer[ soya.sdlconst.K_s ]:
-##            self.m_Camera.MoveBackward()
-##        elif self.m_KeyBuffer[ soya.sdlconst.K_w ]:
-##            self.m_Camera.MoveForward()
-##        else:
-##            self.m_Camera.speed.z = 0.0
-##
-##        if self.m_KeyBuffer[ soya.sdlconst.K_f ]:
-##            self.m_Camera.speed.y = -1.0
-##        elif self.m_KeyBuffer[ soya.sdlconst.K_r ]:
-##            self.m_Camera.speed.y = 1.0
-##        else:
-##            self.m_Camera.speed.y = 0.0
-##
-##        if self.m_KeyBuffer[ soya.sdlconst.K_DOWN ]:
-##            self.m_Camera.rot_x = -10.0
-##        elif self.m_KeyBuffer[ soya.sdlconst.K_UP ]:
-##            self.m_Camera.rot_x = 10.0
-##        else:
-##            self.m_Camera.rot_x = 0.0
-##
-##        if self.m_KeyBuffer[ soya.sdlconst.K_LEFT ]:
-##            self.m_Camera.rot_y = -10.0
-##        elif self.m_KeyBuffer[ soya.sdlconst.K_RIGHT ]:
-##            self.m_Camera.rot_y = 10.0
-##        else:
-##            self.m_Camera.rot_y = 0.0
-
-        '''elif self.m_KeyBuffer[ soya.sdlconst.K_DOWN ]:
-            self.m_Camera.m_XRot -= 1.0
-
-        elif self.m_KeyBuffer[ soya.sdlconst.K_LEFT ]:
-            self.m_Camera.m_YRot -= 1.0
-
-        elif self.m_KeyBuffer[ soya.sdlconst.K_RIGHT ]:
-            self.m_Camera.m_YRot += 1.0
-
-        elif self.m_KeyBuffer[ K_u ]:
-            x, y, z, w = self.m_Ground.GetPosition()
-            y += 0.25
-            self.m_Ground.SetPosition( x, y, z, w )
-            glLightfv( GL_LIGHT0, GL_POSITION, [ x, y, z ] )
-            self.AddMessage( "Position: %s,%s,%s" % ( x, y, z ) )
-
-        elif self.m_KeyBuffer[ K_j ]:
-            x, y, z, w = self.m_Ground.GetPosition()
-            y -= 0.25
-            self.m_Ground.SetPosition( x, y, z, w )
-            glLightfv( GL_LIGHT0, GL_POSITION, [ x, y, z ] )
-            self.AddMessage( "Position: %s,%s,%s" % ( x, y, z ) )
-
-        elif self.m_KeyBuffer[ K_w ]:
-            self.m_Camera.MoveForward()
-
-        elif self.m_KeyBuffer[ K_s ]:
-            self.m_Camera.MoveBackward()
-
-        elif self.m_KeyBuffer[ K_a ]:
-            self.m_Camera.MoveLeft()
-
-        elif self.m_KeyBuffer[ K_d ]:
-            self.m_Camera.MoveRight()
-
-        elif self.m_KeyBuffer[ K_z ]:
-            self.m_Camera.ZoomIn()
-
-        elif self.m_KeyBuffer[ K_x ]:
-            self.m_Camera.ZoomOut()
-
-        elif self.m_KeyBuffer[ K_c ]:
-            x, y, z, w = self.m_Camera.GetPosition()
-            self.AddMessage( "Camera Position: %s,%s,%s" % ( x, y, z ) )
-            self.AddMessage( "angles: %s,%s,%s" % ( self.m_Camera.m_XRot.GetAngle() , 
-                                                    self.m_Camera.m_YRot.GetAngle(), 
-                                                    self.m_Camera.m_ZRot.GetAngle() ) )
-
-        elif self.m_KeyBuffer[ K_r ]:
-            x, y, z, w = self.m_Camera.GetPosition()
-            y -= 0.25
-            self.m_Camera.SetPosition( x, y, z, w )
-
-        elif self.m_KeyBuffer[ K_f ]:
-            x, y, z, w = self.m_Camera.GetPosition()
-            y += 0.25
-            self.m_Camera.SetPosition( x, y, z, w )
-
-        elif self.m_KeyBuffer[ K_e ]:
-            self.m_Mode = EDIT_MODE
-
-        elif self.m_KeyBuffer[ K_q ]:
-            self.m_Mode = NORMAL_MODE
-
-        elif self.m_KeyBuffer[ K_t ]:
-            self.rgn.ToggleWalls()
-        '''
-
     def PrintToConsole(self, a_Message, a_LineNumber ):
         '''
         Print a message to the console on a specific line
         @param a_Message String : a message to display
         @param a_LineNumber Integer: the line to pront your message
         '''
-        #self.font.glPrint( 10 , self.m_Camera.m_ViewportHeight - ( 17 * a_LineNumber ), a_Message )
-
 
     def AddMessage( self, a_Message ):
         '''
--- la_vida.py
+++ la_vida.py
@@ -1,18 +1,21 @@
 #!/usr/env python
 
 import sys, os, os.path, soya
-import GameApp
 from GameApp.game_app_3d import GameApp3d
-from lavida_config import *
-from game_config import *
+from lavida_config import getDataPath
+from game_config import GameConfig
 
 
 if __name__ == "__main__":
     DataPath = getDataPath()
     soya.path.append( DataPath )
     config_file = GameConfig( "%s/la_vida.cfg" % DataPath.replace("/data", "" ) )
-    scrn_res = config_file.ScreenResolution()
+    # get config file
+    # use config file for screen resolution
+    # use config file for screen mode (full screen or not)
+    # make scene instance
+    # use library loop structure
+    screen_resolution = config_file.ScreenResolution()
     fullscreen = config_file.ScreenMode()
-    scene = GameApp3d( scrn_res[ 0 ], scrn_res[ 1 ], fullscreen, 
-                       a_AppName="La Vida", a_DataPath=DataPath )
-    soya.MainLoop( scene ).main_loop()
+    la_vida = GameApp3d()
+    la_vida.run()
