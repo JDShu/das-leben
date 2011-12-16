03b0b226edb9c44015223c954ddb467a74ec14cc
Hans Lo <hansshulo@gmail.com>
2011-12-16 01:28:05 -0500

    Remove legacy comments.
    We now know what we need and don't need.

--- la_vida.py
+++ la_vida.py
@@ -1,21 +1,7 @@
 #!/usr/env python
 
-import sys, os, os.path, soya
 from GameApp.game_app_3d import GameApp3d
-from lavida_config import getDataPath
-from game_config import GameConfig
-
 
 if __name__ == "__main__":
-    DataPath = getDataPath()
-    soya.path.append( DataPath )
-    config_file = GameConfig( "%s/la_vida.cfg" % DataPath.replace("/data", "" ) )
-    # get config file
-    # use config file for screen resolution
-    # use config file for screen mode (full screen or not)
-    # make scene instance
-    # use library loop structure
-    screen_resolution = config_file.ScreenResolution()
-    fullscreen = config_file.ScreenMode()
     la_vida = GameApp3d()
     la_vida.run()
