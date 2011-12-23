b61c34432e0d0970ce61f8f5dd06c67016993dcc
Hans Lo <hansshulo@gmail.com>
2011-12-23 02:16:29 -0500

    Simple gui panel for selecting characters.
    Ready for writing event handlers.

--- das_leben/gui.py
+++ das_leben/gui.py
@@ -5,17 +5,37 @@ import global_functions
 
 class GuiManager:
     
-    def __init__(self):
+    def __init__(self, game_data):
         self.onscreen_title = OnscreenText(text="Das Leben", pos=(0.95,-0.95), 
                                   scale=0.07, fg=(1,0.5,0.5,1),
                                   align=TextNode.ACenter)
-
-        self.quit_dialog = YesNoDialog(dialogName="YesNoCancelDialog",
+        self.quit_dialog = YesNoDialog(dialogName="Quit",
                                        text="Do you want to quit?",
                                        command=self.quit_confirm)
+        self.main_panel = DirectFrame(frameColor=(0.5, 0.2, 0, 1),
+                                      frameSize=(0, 2.2, 0, 0.3),
+                                      pos=(-1.1, 0, -1))
+        self.character_buttons = []
+        catalog = game_data.character_catalog.get_catalog()
+        self.generate_character_buttons(catalog)
+
+    def generate_character_buttons(self, catalog):
+        position_id = 0
+        x = 0.3
+        z = 0.15
+        for character_id, character in catalog.items():
+            button = DirectButton(text = character.profile.name,
+                                  pos=(x,0,z),
+                                  frameSize=(-2,2,-1,1),
+                                  scale=0.1)
+            button.reparentTo(self.main_panel)
+            self.character_buttons.append(button)
+            x += 0.4
+            position_id += 1
+                    
         self.quit_dialog.hide()
         self.dialogs_open = False
-        
+    
     def quit_confirm(self, confirmed):
         if confirmed:
             global_functions.quit()
